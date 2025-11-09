990a9ae39eeba6477e45fe5eb20c9cdd412dde6c
ricardobossan <ricardobossan@protonmail.com>
2025-11-09 00:04:00 -0300

    Configures rosln server to install automatically

--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -1,15 +1,26 @@
--- TODO: Automate language server setup
 return {
-  "seblyng/roslyn.nvim",
-  ft = "cs",
-  ---@module 'roslyn.config'
-  ---@type RoslynNvimConfig
-  opts = {
-    require("mason").setup({
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
+  {
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      require("mason").setup({
+        registries = {
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
+        },
+      }),
+    },
+  },
+
+  -- Ensure roslynv4 is installed
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "roslynv4",
       },
-    }),
+    },
   },
 }
--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 
 ~Life~ Arch is Life!
 
-<!--TODO: Install and configure waybar, to show workspaces and which is selected currently-->
-<!--TODO: Research and install wallpaper manager for wayland, or stick to styli.sh-->
 <!--TODO: Configure dwl key bindings-->
-<!--TODO: Rice roslyn server setup for neovim locally in bootstrap.sh -->
 <!--TODO: automate dwl installation from docs into the bootstrap script-->
