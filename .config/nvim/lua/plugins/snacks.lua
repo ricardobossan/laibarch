743703c5f22ccaed777c58f0cca5ae2b9d5c944f
ricardobossan <ricardobossan@protonmail.com>
2025-12-01 11:42:27 -0300

    Neovim file explorer shows hidden and ignored by default

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true, -- Show hidden files by default
+          ignored = true, -- Show ignored files by default
+        },
+      },
+    },
+  },
+}
