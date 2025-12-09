fc74ff8f8d2072f5fc56df5f19abc5502032b4cb
ricardobossan <ricardobossan@protonmail.com>
2025-12-08 23:21:36 -0300

    Changes AI/Completion defaults to disabled

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,14 +1,13 @@
 return {
-  --[[
   {
     "Exafunction/codeium.nvim",
     commit = "821b570b526dbb05b57aa4ded578b709a704a38a",
-    enabled = true,
+    enabled = false,
   },
   {
     "zbirenbaum/copilot.lua",
     commit = "d661d65b4cab20a5c164f6d9081d91ed324fe4d8",
-    enabled = true,
+    enabled = false,
   },
   {
     "tzachar/cmp-tabnine",
@@ -18,7 +17,6 @@ return {
   {
     "saghen/blink.cmp",
     commit = "49f211fe5d729df53df4c042d7c3464cf47d199e",
-    enabled = true,
+    enabled = false,
   },
-  --]]
 }
