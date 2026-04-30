7bde97ebbf534b8021e3da7826f83aa8767d4db4
ricardobossan <ricardobossan@protonmail.com>
2026-04-29 21:56:26 -0300

    Re-enable AI completion

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.ai_cmp = true -- enable AI completion sources in blink.cmp / nvim-cmp
+
 vim.opt.wrap = true
 
 -- Override LazyVim's fold settings to use treesitter
--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -2,21 +2,21 @@ return {
   {
     "Exafunction/codeium.nvim",
     commit = "821b570b526dbb05b57aa4ded578b709a704a38a",
-    enabled = false,
+    enabled = true,
   },
   {
     "zbirenbaum/copilot.lua",
     commit = "d661d65b4cab20a5c164f6d9081d91ed324fe4d8",
-    enabled = false,
+    enabled = true,
   },
   {
     "tzachar/cmp-tabnine",
     commit = "c0167cdc86c15e782c5461ee62aebee89231c2ed",
-    enabled = false,
+    enabled = true,
   },
   {
     "saghen/blink.cmp",
     commit = "49f211fe5d729df53df4c042d7c3464cf47d199e",
-    enabled = false,
+    enabled = true,
   },
 }
