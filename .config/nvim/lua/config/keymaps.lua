272971a402d6661e1e0774c5d3d4efd90869df3a
ricardobossan <ricardobossan@protonmail.com>
2026-05-06 17:36:09 -0300

    Removes custom o and O bindings from neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,7 +10,7 @@ map("v", "jk", "<ESC>", default_opts)
 map("t", "jk", "<ESC>", default_opts)
 
 -- New line escapes insert
-map("n", "o", "o<ESC>", default_opts)
-map("v", "o", "o<ESC>", default_opts)
-map("n", "O", "O<ESC>", default_opts)
-map("v", "O", "O<ESC>", default_opts)
+-- map("n", "o", "o<ESC>", default_opts)
+-- map("v", "o", "o<ESC>", default_opts)
+-- map("n", "O", "O<ESC>", default_opts)
+-- map("v", "O", "O<ESC>", default_opts)
